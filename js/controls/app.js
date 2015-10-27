905568fcaad90972eadeb5475761236573cdb76e
Nate Johnson <njohnson@cars.com>
2015-10-26 20:42:36 -0500

    Add JSON fixture

--- js/controls/app.js
+++
@@ -1,9 +0,0 @@
-define(['jquery', 'can', 'js/controls/invoice.js', 'domReady!'], function($, can, Invoice) {
-	return can.Control({
-		init : function(el) {
-			console.debug('el', el);
-			
-			var i = new Invoice('', {id : 5});
-		}
-	});
-});
