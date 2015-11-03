4c90239b0828ee90c10e2aa353626a5df7ca3be7
Nate Johnson <njohnson@cars.com>
2015-11-02 20:30:43 -0600

    Add HAPI

--- services/app.js
+++ services/app.js
@@ -1,15 +1,23 @@
+/**
+ * @author Nate Johnson
+ * @type {*|exports|module.exports}
+ */
+
 var restify = require('restify');
 
 var server = restify.createServer({
     name: 'invoices',
     version: '1.0.0'
 });
+
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
 server.get('/invoice/:invoice-no', function (req, res, next) {
-    res.send(req.params);
+    //res.send(req.params);
+    var invoice = { blah: "whee" };
+    res.send(invoice);
     return next();
 });
 
